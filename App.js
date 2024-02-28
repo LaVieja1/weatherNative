3f1c8330206b149678854257c8ac1b5f2d39f220
santicano <canosantiago404@gmail.com>
2024-02-28 12:47:21 -0300

    estructura principal de pantalla

--- App.js
+++ App.js
@@ -5,7 +5,16 @@ const App = () => {
   return (
     <SafeAreaView style={styles.wrapper}>
       <View style={styles.container}>
-        <Text>Current Weather</Text>
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feels}>Se siente como 5</Text>
+        <View style={styles.highLowWrapper}>
+          <Text style={styles.highLow}>Alta: 8</Text>
+          <Text style={styles.highLow}>Baja: 6</Text>
+        </View>
+      </View>
+      <View style={styles.bodyWrapper}>
+        <Text style={styles.description}>Esta soleado</Text>
+        <Text style={styles.message}>Perfecto para una remera</Text>
       </View>
     </SafeAreaView>
   );
@@ -14,11 +23,37 @@ const App = () => {
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
+    backgroundColor: "pink",
   },
   container: {
-    backgroundColor: "pink",
+    paddingTop: 40,
     flex: 1,
     alignItems: "center",
+  },
+  temp: {
+    color: "black",
+    fontSize: 48,
+  },
+  feels: {
+    fontSize: 30,
+    color: "black",
+  },
+  highLowWrapper: {
+    flexDirection: "row",
+  },
+  highLow: {
+    color: "black",
+    fontSize: 20,
+  },
+  bodyWrapper: {
+    justifyContent: "flex-end",
+    alignItems: "flex-start",
+  },
+  description: {
+    fontSize: 48,
+  },
+  message: {
+    fontSize: 20,
   },
 });
 

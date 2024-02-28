7b566369e6c54e2de6df4b9464f51020e04772a5
santicano <canosantiago404@gmail.com>
2024-02-28 14:55:41 -0300

    creado componente y agregado icon

--- App.js
+++ App.js
@@ -1,59 +1,18 @@
 import React from "react";
-import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import CurrentWeather from "./src/components/CurrentWeather";
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.wrapper}>
-      <View style={styles.container}>
-        <Text style={styles.temp}>6</Text>
-        <Text style={styles.feels}>Se siente como 5</Text>
-        <View style={styles.highLowWrapper}>
-          <Text style={styles.highLow}>Alta: 8</Text>
-          <Text style={styles.highLow}>Baja: 6</Text>
-        </View>
-      </View>
-      <View style={styles.bodyWrapper}>
-        <Text style={styles.description}>Esta soleado</Text>
-        <Text style={styles.message}>Perfecto para una remera</Text>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <CurrentWeather />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  wrapper: {
-    flex: 1,
-    backgroundColor: "pink",
-  },
   container: {
-    paddingTop: 40,
     flex: 1,
-    alignItems: "center",
-  },
-  temp: {
-    color: "black",
-    fontSize: 48,
-  },
-  feels: {
-    fontSize: 30,
-    color: "black",
-  },
-  highLowWrapper: {
-    flexDirection: "row",
-  },
-  highLow: {
-    color: "black",
-    fontSize: 20,
-  },
-  bodyWrapper: {
-    justifyContent: "flex-end",
-    alignItems: "flex-start",
-  },
-  description: {
-    fontSize: 48,
-  },
-  message: {
-    fontSize: 20,
   },
 });
 

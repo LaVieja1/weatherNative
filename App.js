b82b33ca943f31de30943f40a62dadaf87c8a2f1
santicano <canosantiago404@gmail.com>
2024-02-29 11:40:01 -0300

    añadido loading state

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { View, ActivityIndicator, StyleSheet } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 
 import Tabs from './src/components/Tabs'
 
 const App = () => {
+  const [loading, setLoading] = useState(true)
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={'large'} color={'blue'} />
+      </View>
+    )
+  }
+
   return (
     <NavigationContainer>
       <Tabs />
     </NavigationContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  }
+})
 
 export default App

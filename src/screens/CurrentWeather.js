f38d2071869ca379ae249e6f87e9e2541dfb339a
santicano <canosantiago404@gmail.com>
2024-02-29 11:24:27 -0300

    añadido weatherType utilities

--- src/screens/CurrentWeather.js
+++ src/screens/CurrentWeather.js
@@ -1,7 +1,10 @@
-import React from "react";
-import { View, Text, SafeAreaView, StyleSheet } from "react-native";
-import { Feather } from "@expo/vector-icons";
-import RowText from "../components/RowText";
+import React from 'react'
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+
+import { weatherType } from '../utilities/weatherType'
+
+import RowText from '../components/RowText'
 
 const CurrentWeather = () => {
   const {
@@ -13,8 +16,8 @@ const CurrentWeather = () => {
     highLow,
     bodyWrapper,
     description,
-    message,
-  } = styles;
+    message
+  } = styles
 
   return (
     <SafeAreaView style={wrapper}>
@@ -23,62 +26,62 @@ const CurrentWeather = () => {
         <Text style={temp}>6</Text>
         <Text style={feels}>Se siente como 5</Text>
         <RowText
-          messageOne={"Alta: 8"}
-          messageTwo={"Baja: 6"}
+          messageOne={'Alta: 8'}
+          messageTwo={'Baja: 6'}
           containerStyles={highLowWrapper}
           messageOneStyles={highLow}
           messageTwoStyles={highLow}
         />
       </View>
       <RowText
-        messageOne={"Esta soleado"}
-        messageTwo={"Perfecto para una remera"}
+        messageOne={'Esta soleado'}
+        messageTwo={weatherType['Thunderstorm'].message}
         containerStyles={bodyWrapper}
         messageOneStyles={description}
         messageTwoStyles={message}
       />
     </SafeAreaView>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
-    backgroundColor: "pink",
+    backgroundColor: 'pink'
   },
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   temp: {
-    color: "black",
-    fontSize: 48,
+    color: 'black',
+    fontSize: 48
   },
   feels: {
     fontSize: 30,
-    color: "black",
+    color: 'black'
   },
   highLowWrapper: {
-    flexDirection: "row",
-    gap: 5,
+    flexDirection: 'row',
+    gap: 5
   },
   highLow: {
-    color: "black",
-    fontSize: 20,
+    color: 'black',
+    fontSize: 20
   },
   bodyWrapper: {
-    justifyContent: "flex-end",
-    alignItems: "flex-start",
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
     paddingLeft: 25,
-    marginBottom: 40,
+    marginBottom: 40
   },
   description: {
-    fontSize: 48,
+    fontSize: 48
   },
   message: {
-    fontSize: 20,
-  },
-});
+    fontSize: 20
+  }
+})
 
-export default CurrentWeather;
+export default CurrentWeather
---
+++ src/utilities/weatherType.js
@@ -0,0 +1,42 @@
+export const weatherType = {
+  Thunderstorm: {
+    icon: 'zap',
+    message: 'Podría ser ruidoso',
+    backgroundColor: '#000000'
+  },
+  Drizzle: {
+    icon: 'cloud-rain',
+    message: 'Podría llover un poco',
+    backgroundColor: '#36454f'
+  },
+  Rain: {
+    icon: 'umbrella',
+    message: 'Vas a necesitar un paraguas',
+    backgroundColor: '#0000ff'
+  },
+  Snow: {
+    icon: 'cloud-snow',
+    message: 'Hagamos un muñeco de nieve',
+    backgroundColor: '#7f6065'
+  },
+  Clear: {
+    icon: 'sun',
+    message: 'Esta perfecto para una remera',
+    backgroundColor: '#e47200'
+  },
+  Clouds: {
+    icon: 'cloud',
+    message: 'Podriás vivir en las nubes',
+    backgroundColor: '#363636'
+  },
+  Haze: {
+    icon: 'wind',
+    message: 'Podría llegar a estar húmedo',
+    backgroundColor: '#58586e'
+  },
+  Mist: {
+    icon: 'align-justify',
+    message: 'Podría ser difícil ver',
+    backgroundColor: '#3e3e37'
+  }
+}

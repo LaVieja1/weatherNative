1025859e55664eab2875adc0855c23a793cc4beb
santicano <canosantiago404@gmail.com>
2024-03-01 14:02:47 -0300

    añadido componente error

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { useGetWeather } from './src/hooks/useGetWeather'
 
 import Tabs from './src/components/Tabs'
+import ErrorItem from './src/components/ErrorItem'
 
 const App = () => {
   const [loading, error, weather] = useGetWeather()
@@ -19,7 +20,11 @@ const App = () => {
 
   return (
     <View style={styles.container}>
-      <ActivityIndicator size={'large'} color={'blue'} />
+      {loading ? (
+        <ActivityIndicator size={'large'} color={'blue'} />
+      ) : (
+        <ErrorItem />
+      )}
     </View>
   )
 }

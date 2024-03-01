fc01bbabb0dd33ff5993b16dfedd3d4a579461e7
santicano <canosantiago404@gmail.com>
2024-03-01 10:09:30 -0300

    creado hook useGetWeather

--- App.js
+++ App.js
@@ -1,51 +1,14 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { View, ActivityIndicator, StyleSheet } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
-import * as Location from 'expo-location'
-import { WEATHER_API_KEY } from '@env'
+
+import { useGetWeather } from './src/hooks/useGetWeather'
 
 import Tabs from './src/components/Tabs'
 
 const App = () => {
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(null)
-  const [weather, setWeather] = useState([])
-  const [lat, setLat] = useState([])
-  const [lon, setLon] = useState([])
-
-  const fetchWeatherData = async () => {
-    try {
-      const res = await fetch(
-        `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
-      )
-      const data = await res.json()
-      setWeather(data)
-    } catch (error) {
-      setError('Could not fetch weather data')
-    } finally {
-      setLoading(false)
-    }
-  }
-
-  useEffect(() => {
-    ;(async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync()
-
-      if (status !== 'granted') {
-        setError('Permiso para acceder a la ubicación denegado')
-        return
-      }
-
-      let location = await Location.getCurrentPositionAsync({})
-      setLat(location.coords.latitude)
-      setLon(location.coords.longitude)
-      await fetchWeatherData()
-    })()
-  }, [lat, lon])
-
-  if (weather) {
-    console.log(weather)
-  }
+  const [loading, error, weather] = useGetWeather()
+  console.log(loading, error, weather)
 
   if (loading) {
     return (

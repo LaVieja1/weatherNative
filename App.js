7957615b8bb0cb3287cc5a34470629a86c40cf4d
santicano <canosantiago404@gmail.com>
2024-03-01 10:01:07 -0300

    fetch api data y console log

--- App.js
+++ App.js
@@ -8,8 +8,24 @@ import Tabs from './src/components/Tabs'
 
 const App = () => {
   const [loading, setLoading] = useState(false)
-  const [location, setLocation] = useState(null)
   const [error, setError] = useState(null)
+  const [weather, setWeather] = useState([])
+  const [lat, setLat] = useState([])
+  const [lon, setLon] = useState([])
+
+  const fetchWeatherData = async () => {
+    try {
+      const res = await fetch(
+        `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+      )
+      const data = await res.json()
+      setWeather(data)
+    } catch (error) {
+      setError('Could not fetch weather data')
+    } finally {
+      setLoading(false)
+    }
+  }
 
   useEffect(() => {
     ;(async () => {
@@ -21,12 +37,14 @@ const App = () => {
       }
 
       let location = await Location.getCurrentPositionAsync({})
-      setLocation(location)
+      setLat(location.coords.latitude)
+      setLon(location.coords.longitude)
+      await fetchWeatherData()
     })()
-  }, [])
+  }, [lat, lon])
 
-  if (location) {
-    console.log(location)
+  if (weather) {
+    console.log(weather)
   }
 
   if (loading) {
